1f3596e8885439e325e03ef6489b93de557d0d15
Romadhan Prasetyo <romadhanedy@gmail.com>
2020-10-30 02:07:18 +0700

    :zap: Update splashscreen

--- components/SplashScreen.vue
+++ components/SplashScreen.vue
@@ -1,20 +1,19 @@
 <template>
-    <div class="bg-dark position-fixed d-flex justify-content-center align-items-center flex-column" style="width: 100%; height: 100%">
-        <span class="h1 text-uppercase font-weight-bold">
-            <span class="text-success">Nuxt</span>
-            <i class="far fa-heart"></i>
-            <span class="text-primary">Stisla</span>
-        </span>
-        <button class="btn btn-outline-success mt-3" @click="getStarted()">Get Started</button>
-    </div>
+  <div
+    class="bg-dark position-fixed d-flex justify-content-center align-items-center flex-column"
+    style="width: 100%; height: 100%"
+  >
+    <a href="https://github.com/dyprast" target="blank" class="text-uppercase font-weight-bold">
+        <i class="fas fa-fire"></i> Source Code<i class="fas fa-fire"></i>
+    </a>
+    <span class="h1 text-uppercase text-white font-weight-bold mt-5">
+      <span>Nuxt</span>
+      <span>Stisla</span>
+    </span>
+    <a href="/dashboard/ecommerce" class="btn btn-lg bg-primary text-white mt-5 text-uppercase font-weight-bold">Get Started</a>
+  </div>
 </template>
 
 <script>
-export default {
-    methods: {
-        getStarted() {
-            this.$router.push('/dashboard/ecommerce')
-        }
-    }
-}
+export default {};
 </script>
